7f4f63dc678ca119dfc62f3db584a063390875c0
Esteban FM <esteban.fsma@gmail.com>
2024-08-09 17:46:26 -0600

    Add titulo

---
+++ components/Titulo.js
@@ -0,0 +1,29 @@
+
+import { useState } from 'react';
+
+export default function TItulo() {
+ 
+    return(
+            <>
+
+<div className="relative bg-zinc-50 dark:bg-black flex justify-center items-center ">
+
+<div className="sm:pt-5 mx-auto grid max-w-7xl grid-cols-10  ">
+
+
+
+    <div className=" col-start-1 col-span-10 sm:-mt-10  ">
+                <div className="hero-content lg:flex-row ">
+                    <img src="https://res.cloudinary.com/deeg8ig3s/image/upload/v1723247118/fwk8i997mteq4ooasvgb.png" className=" rounded-lg " />
+                </div>
+    </div>
+
+
+          
+
+</div>
+</div>
+
+</>
+);
+}
--- pages/contactanos.js
+++ pages/contactanos.js
@@ -5,6 +5,7 @@ import Card from "@/components/Card";
 import Video from "@/components/Video";
 import Proceso from "@/components/Proceso";
 import Chulo from "@/components/Chulo";
+import TItulo from "@/components/Titulo";
 
 export default function Contactanos() {
     
@@ -15,6 +16,7 @@ export default function Contactanos() {
                 <main>
 
                     <Navbar/>
+                    <TItulo/>
                     <Proceso/>
                     <Chulo/>
                     <AnotherForm />
