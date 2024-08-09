5075223c1373ac6eb735fecf60f58451d37d5486
Esteban FM <esteban.fsma@gmail.com>
2024-08-09 16:30:31 -0600

    Add chulo divider

---
+++ components/Chulo.js
@@ -0,0 +1,20 @@
+
+export default function Chulo() {
+ 
+    return(
+        <>
+
+<hr className="my-2 border-t-2 border-zinc-800" />
+
+        <section className=" text-white flex-1 flex-wrap grid grid-cols-5 items-center gap-4 flex justify-center text-center">
+
+            <div className=" col-span-1 col-start-2">
+                <a className="absolute  font-bold text-2xl hover:text-zinc-400" href="https://github.com/Esteban-FM">👍 Tal vez un tatuaje te haga ver mas chulo 👍</a>
+            </div>
+
+        </section>    
+
+<hr className="my-6 border-t-2 border-zinc-800" />
+</>
+);
+}
--- pages/contactanos.js
+++ pages/contactanos.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Card from "@/components/Card";
 import Video from "@/components/Video";
 import Proceso from "@/components/Proceso";
+import Chulo from "@/components/Chulo";
 
 export default function Contactanos() {
     
@@ -15,6 +16,7 @@ export default function Contactanos() {
 
                     <Navbar/>
                     <Proceso/>
+                    <Chulo/>
                     <AnotherForm />
                     <Video/>
                     <Card/>
