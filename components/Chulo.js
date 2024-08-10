dbffdafa3af1f48d2e0d8a00215f79d4b12bb116
Esteban FM <esteban.fsma@gmail.com>
2024-08-09 18:21:57 -0600

    Edit Chulo

--- components/Chulo.js
+++ components/Chulo.js
@@ -6,9 +6,9 @@ export default function Chulo() {
 
 <hr className="my-2 border-t-2 border-zinc-800" />
 
-        <section className=" text-white flex-1 flex-wrap grid grid-cols-6 items-center gap-4 flex justify-center text-center">
+        <section className=" text-white flex-1 flex-wrap grid grid-cols-7 items-center gap-4 flex justify-center text-center">
 
-            <div className=" col-span-2 col-start-3">
+            <div className=" col-span-3 col-start-3">
                 <a className="font-bold text-2xl hover:text-zinc-400" href="">👍Tal vez un tatuaje te haga ver mas chulo👍</a>
             </div>
 
